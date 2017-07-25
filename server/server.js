f9817057d82bd938268c175de5380dd9daeb4094
Joe <joe.liang36@gmail.com>
2017-07-24 23:41:58 -0700

    Added GET /todo/:id route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 var {moongose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb')
+
 
 var app = express();
 
@@ -26,6 +28,24 @@ app.get('/todos', (req, res) => {
     res.send({todos})
   }, (e) => {
     res.status(400).send(err);
+  });
+});
+
+// GET /todos/#Number
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  };
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }, (e) => {
+    res.status(400).send();
   });
 });
 
